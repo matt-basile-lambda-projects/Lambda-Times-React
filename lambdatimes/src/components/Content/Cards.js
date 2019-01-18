9a1a48500ed48cb785c3024dc73aaebe7c2bce49
Matt Basile <matthewbasile2@gmail.com>
2019-01-18 12:43:56 -0500

    Add PropType checkers.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   console.log(props.cards)
@@ -8,12 +9,17 @@ const Cards = props => {
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
       {props.cards.map(card =>{
-        return <Card card={card} />
+        return <Card 
+        key={card.headline}
+        card={card} />
       })}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes={
+  cards: PropTypes.array,
+}
 
 export default Cards;
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -23,7 +23,7 @@ const Tab = props => {
 
 // Make sure you include PropTypes on your props.
 Tab.propTypes={
-  tab: PropTypes.string,
+  tab:PropTypes.string,
   selectTabHandler: PropTypes.function,
   selectedTab: PropTypes.string
 }
